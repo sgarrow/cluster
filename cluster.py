5014d087e4d85e868912d8bc0ca82909ad9c9091
sgarrow <stangarrow@yahoo.com>
2016-07-31 17:20:00 -0700

    works

--- cluster.py
+++ cluster.py
@@ -1,4 +1,6 @@
+import time
 import pprint
+
 # Make a Bad-Pixel-List from a Bad-Pixel-Map.
 def makeBplFromBpm( bpm ):
     rows = len( bpm )
@@ -8,7 +10,18 @@ def makeBplFromBpm( bpm ):
         for jj in range( cols ):
             if bpm[ii][jj]==0:
                 bpl.append( (ii,jj) )
+
     return bpl
+#############################################
+
+# Pretty Print bcl.
+def printbcl( inbcl ):
+    for ii,c in enumerate(inbcl):
+        print('{:4d}: '.format(ii), end = '')
+        for bp in c:
+            print(bp, end='')
+        print()
+    print()
 #############################################
 
 # Determine if the passed in pix touches any pix in the passed in cluster.
@@ -38,44 +51,40 @@ def makeBclFromBpl( bpl ):
                 pixBelongs2Clstrs.append( ii )
                 if len( pixBelongs2Clstrs ) == 2:
                     break
-        #print( pixBelongs2Clstrs )
 
         # Insert this BP into a new Cluster.  
         if len( pixBelongs2Clstrs ) == 0:
             bcl.append( [ currBp ] )
-            myStr = 'pixel {} creates   new cluster {}: {}\n'
-            print(myStr.format( currBp, len(bcl)-1, bcl[-1] ))
+            #myStr = 'pixel {} creates new cluster {}'
+            #print(myStr.format( currBp, len(bcl)-1 ))
 
         # Add this pixel into a single existing cluster. 
         elif len( pixBelongs2Clstrs ) == 1:
             bcl[pixBelongs2Clstrs[ 0 ]].append( currBp )
-            myStr = 'pixel {} inserted into cluster {}: {}\n'
-            print(myStr.format( currBp, pixBelongs2Clstrs[ 0 ], bcl[pixBelongs2Clstrs[ 0 ]] ))
-            pprint.pprint(bcl)
+            #myStr = 'pixel {} inserted into cluster {}'
+            #print(myStr.format( currBp, pixBelongs2Clstrs[ 0 ] ))
 
-        # Create new combined cluster, delete old combined clusters then add this pixel to it.
+        # Combine clusters, add pixel to combined, 
+        # delete old clusters, add new cluster to bcl.
         elif len( pixBelongs2Clstrs ) == 2:
 
-            set1 = set( bcl[ pixBelongs2Clstrs[ 0 ] ] ) # Combine clusters to single cluster.
+            set1 = set( bcl[ pixBelongs2Clstrs[ 0 ] ] ) # Combine.
             set2 = set( bcl[ pixBelongs2Clstrs[ 1 ] ] )
             set3 = set1.union( set2 )
             tmpLst  = list( set3 )
 
-            myStr = 'Combined clusters {}: {}'
-            print(myStr.format( pixBelongs2Clstrs[ 0 ], pixBelongs2Clstrs[ 1 ] ))
+            tmpLst.append( currBp )                     # Append pixel.
+            bcl.append( tmpLst )                        # Append cluster.
 
-            myStr = 'Deleted clusters {}: {}'
-            print(myStr.format( pixBelongs2Clstrs[ 0 ], pixBelongs2Clstrs[ 1 ] ))
-            del bcl[ pixBelongs2Clstrs[ 0 ] ]           # Remove combined clusters.
+            del bcl[ pixBelongs2Clstrs[ 0 ] ]           # Delete.
             del bcl[ pixBelongs2Clstrs[ 1 ] - 1 ]
 
-            bcl.append( tmpLst )
+            #myStr = 'pixel {} caused a combine/append/delete action of  clusters {} and {}'
+            #print(myStr.format( currBp, pixBelongs2Clstrs[ 0 ], pixBelongs2Clstrs[ 1 ] ))
 
-            mtYtr = 'Appended combined cluster {} to bcl {}'
-            print(myStr.format( tmpLst, bcl ))
-
-        else:
-            print('ERROR')
+        #print( 'bcl after handling pixel {} is:'.format(currBp))
+        #printbcl(bcl)
+        #x = input()
 
     return bcl
 
@@ -89,15 +98,11 @@ if __name__ == '__main__':
     # 
     # A Bad-Pixel-Map (bpm) is a 2D array of ints, like this:
     # 
-    #           0  1  2  3  4  5  6  7  
-    #       [ [ 0, 0, 0, 0, 0, 0, 0, 0 ],  # 0
-    #         [ 0, 0, 0, 1, 1, 0, 0, 0 ],  # 1
-    #         [ 0, 0, 0, 1, 1, 0, 0, 0 ],  # 2
-    #         [ 0, 0, 0, 0, 0, 0, 0, 0 ],  # 3
-    #         [ 0, 0, 0, 0, 0, 0, 0, 0 ],  # 4
-    #         [ 0, 0, 0, 0, 0, 1, 1, 0 ],  # 5
-    #         [ 0, 0, 0, 0, 0, 1, 1, 0 ],  # 6
-    #         [ 0, 0, 0, 0, 0, 0, 0, 0 ] ] # 7
+    #           0  1  2  3   
+    #       [ [ 0, 0, 0, 0  ],   # 0
+    #         [ 0, 0, 0, 1  ],   # 1
+    #         [ 0, 0, 0, 1  ],   # 2
+    #         [ 0, 0, 0, 0  ], ] # 3
     # 
     # A Bad-Pixel-List (bpl) is a 1D array of Tuples, like this:
     #       [ (1, 3), (1, 4), (2, 3), (2, 4),
@@ -108,8 +113,9 @@ if __name__ == '__main__':
     #       [ [ (1, 3), (1, 4), (2, 3), (2, 4) ], 
     #         [ (5, 5), (5, 6), (6, 5), (6, 6)] ]
 
-    for currGbpm in gBpm[0:1]:
+    for jj, currGbpm in enumerate(gBpm):
 
+        startTime = time.process_time()
         mBpl = makeBplFromBpm( currGbpm )
         mBcl = makeBclFromBpl( mBpl )
     
@@ -120,60 +126,44 @@ if __name__ == '__main__':
             maxClusterSize = max(len(c) for c in mBcl)
         else:
             maxClusterSize = 0
+        endTime = time.process_time()
+        elapsedTime = endTime - startTime
     
         hist = [0]*(maxClusterSize+1)
         for c in mBcl:
             hist[len(c)] += 1
     
-        print()    
-        print( ' ************* SUMMARY ********************************************')
+        print()
+            
+        myStr1 = ( ' ' + '*' * 5 + ' SUMMARY TEST CASE: {:2d}.  EXECUTION TIME: {:7.3f} SECONDS. ' + 
+                   '*' * 5).format(jj,elapsedTime)
+        print(myStr1)
+
         print( ' * ')
-        print( ' * Total Number of Bad Pixels: {:4d}'.format(len(mBpl)))
-        print( ' * ')
+        print( ' *         Total Number of Bad Pixels: {:4d} '.format( len(mBpl)         ))
         print( ' *       Number of Singleton Clusters: {:4d} '.format( numClustersLenEq1 ))
         print( ' *   Number of Non-Singleton Clusters: {:4d} '.format( numClustersLenGt1 ))
-        print( ' *           Total Number of Clusters: {:4d} '.format(len(mBcl)))
+        print( ' *           Total Number of Clusters: {:4d} '.format( len(mBcl)         ))
+        print( ' *               Largest Cluster Size: {:4d} '.format( maxClusterSize    ))
         print( ' * ')
-        print( ' *               Largest Cluster Size: {:4d} '.format( maxClusterSize ))
-        print( ' * ')
-        print( ' ******************************************************************')
-        print( ' ************* HISTOGRAM ******************************************')
+        print( ' *   Histogram = \n *' )
     
         for ii in range(1,len(hist)):
             if hist[ii] > 0:
-                print( ' * {:4d} Clusters contain {:4d} Pixels. *'.format( hist[ii], ii ))
+                print( ' * {:5d} Clusters contain {:4d} Pixels.'.format( hist[ii], ii ))
+        print( ' * ')
     
-        print( ' \n******************************************************************\n')
-    
-        singletonCnt = 0
-        for ii in range(len(mBcl)):
-            if len(mBcl[ii])>0:
-                pass
-#                print( '   Cluster {:3} has {:3} pixels: {}'.format( ii, len(mBcl[ii]), mBcl[ii]) )
+        print( ' ' + '*' * 68 )
 
-#*  102 Clusters contain    1 Pixels. * 102 Clusters contain    1 Pixels. * 
-#*   10 Clusters contain    2 Pixels. *  10 Clusters contain    2 Pixels. * 
-#*    9 Clusters contain    3 Pixels. *   9 Clusters contain    3 Pixels. * 
-#*    7 Clusters contain    4 Pixels. *   7 Clusters contain    4 Pixels. * 
-#*    4 Clusters contain    5 Pixels. *   4 Clusters contain    5 Pixels. * 
-#*    1 Clusters contain    6 Pixels. *   1 Clusters contain    6 Pixels. * 
-#*    2 Clusters contain   10 Pixels. *   3 Clusters contain   10 Pixels. * 
-#*    1 Clusters contain   13 Pixels. *   1 Clusters contain   13 Pixels. * 
-#*    1 Clusters contain   15 Pixels. *   1 Clusters contain   15 Pixels. * 
-#*    1 Clusters contain   17 Pixels. *   2 Clusters contain   17 Pixels. * 
-#*    1 Clusters contain   23 Pixels. *   
-#*    1 Clusters contain   25 Pixels. *   1 Clusters contain   25 Pixels. *  
-
-# [[4, 5],
-# [9, 10],
-# [41, 42],
-# [52, 56],
-# [53, 57],
-# [54, 56],
-# [61, 62],
-# [69, 70],
-# [91, 92],
-# [121, 123],
-# [125, 126],
-# [144, 146]]
-
+# 102 Clusters contain    1 Pixels
+#  10 Clusters contain    2 Pixels
+#   9 Clusters contain    3 Pixels
+#   7 Clusters contain    4 Pixels
+#   4 Clusters contain    5 Pixels
+#   1 Clusters contain    6 Pixels
+#   2 Clusters contain   10 Pixels
+#   1 Clusters contain   13 Pixels
+#   1 Clusters contain   15 Pixels
+#   1 Clusters contain   17 Pixels
+#   1 Clusters contain   23 Pixels
+#   1 Clusters contain   25 Pixels
